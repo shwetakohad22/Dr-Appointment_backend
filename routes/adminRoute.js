161a312467726357e1d4ac665317828191d1c9b6
shwetakohad22 <shwetakohad22@gmail.com>
2024-02-28 21:09:03 +0530

    initial comite

---
+++ routes/adminRoute.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel.js");
+const Doctor = require("../models/doctorModel.js");
+const authMiddlewear = require("../middlewears/authMiddlewear.js");
+
+router.get("/get-all-doctors", authMiddlewear, async (req, res) => {
+  try {
+    const doctors = await Doctor.find({});
+    res.status(200).send({
+      message: "Doctors fetched successfully",
+      success: true,
+      data: doctors,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error applying doctor account",
+      success: false,
+      error,
+    });
+  }
+});
+
+router.get("/get-all-users", authMiddlewear, async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send({
+      message: "Users fetched successfully",
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Error applying doctor account",
+      success: false,
+      error,
+    });
+  }
+});
+
+router.post(
+  "/change-doctor-account-status",
+  authMiddlewear,
+  async (req, res) => {
+    try {
+      const { doctorId, status } = req.body;
+      const doctor = await Doctor.findByIdAndUpdate(doctorId, {
+        status,
+      });
+
+      const user = await User.findOne({ _id: doctor.userId });
+      const unseenNotifications = user.unseenNotifications;
+      unseenNotifications.push({
+        type: "new-doctor-request-changed",
+        message: `Your doctor account has been ${status}`,
+        onClickPath: "/notifications",
+      });
+      user.isDoctor = status === "approved" ? true : false;
+      await user.save();
+
+      res.status(200).send({
+        message: "Doctor status updated successfully",
+        success: true,
+        data: doctor,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "Error applying doctor account",
+        success: false,
+        error,
+      });
+    }
+  }
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig.js");
+const userRoute = require("./routes/userRoute.js");
+const port = process.env.PORT || 5000;
+const adminRoute = require("./routes/adminRoute.js");
+const doctorRoute = require("./routes/doctorsRoute.js");
+app.use(express.json());
+
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+
+app.listen(port, () => {
+  console.log(`node server started at port ${port}`);
+});
